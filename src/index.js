79f4f21962aed5a5508d001bdca056d82d6fe483
mahmedbutt <43074599+mahmedbutt@users.noreply.github.com>
2022-09-08 00:37:55 +0500

    latest commit

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/js/bootstrap.min";
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import AlertTemplate from 'react-alert-template-basic'
-import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import AlertTemplate from "react-alert-template-basic";
+import { transitions, positions, Provider as AlertProvider } from "react-alert";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 const options = {
   // you can also just use 'bottom center'
   position: positions.BOTTOM_RIGHT,
   timeout: 5000,
-  offset: '30px',
+  offset: "30px",
   // you can also just use 'scale'
-  transition: transitions.SCALE
-}
+  transition: transitions.SCALE,
+};
 
 root.render(
-  <React.StrictMode>
-     <AlertProvider template={AlertTemplate} {...options}>
+  // <React.StrictMode>
+  <AlertProvider template={AlertTemplate} {...options}>
     <App />
-    </AlertProvider>
-  </React.StrictMode>
+  </AlertProvider>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
